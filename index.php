5b2e25d887f20dd80c5763dbbf14269cb5276f40
Victor <victor@victor.(none)>
2013-05-28 14:24:48 +0200

    Revert "Update index.php"
    This reverts commit 7992d64e5387f5b022ab159143f806d2319341b5.

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-dscedc
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // Funció per a fer una suma
+        ?>
+        
+        <?
+        function suma($a,$b){
+            $resultat = $a + $b;
+            return $resultat;
+        }
+        suma($a, $b);
+
+	function resta($a,$b){
+            $resultat = $a - $b;
+            return $resultat;
+        }
+        resta($a, $b);
+        ?>
+    </body>
+</html>
