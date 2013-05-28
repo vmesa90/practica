4cd4e73cac967d125bcb8555711ef308f4199c0d
Victor <victor@victor.(none)>
2013-05-28 13:05:23 +0200

    prueba

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // Versió inicial del projecte
-        ?>
-    </body>
-</html>
+34f3
