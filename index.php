e455239ba1216bf38d5844ae649f39ea9cbd334e
Victor <victor@victor.(none)>
2013-05-28 14:09:06 +0200

    suma modificado index.php

--- index.php
+++ index.php
@@ -15,6 +15,12 @@
             return $resultat;
         }
         suma($a, $b);
+
+	function resta($a,$b){
+            $resultat = $a - $b;
+            return $resultat;
+        }
+        resta($a, $b);
         ?>
     </body>
 </html>
